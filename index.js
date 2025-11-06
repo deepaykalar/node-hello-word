fe00917daf02cb125f9dca4555876eb67a1ec6d5
Deepa <deepaykalar@gmail.com>
2025-11-06 22:06:58 +0530

    add home route

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ app.get("/",(req,res)=>{
     res.send("hello world")
 });
 
-app.listen(5100, (err) => {n
+app.get("/home",(req,res)=>{
+    res.send("Home page")
+})
+
+app.listen(5100, (err) => {
     if (err) console.log(err)
     else console.log(5100)
 })
